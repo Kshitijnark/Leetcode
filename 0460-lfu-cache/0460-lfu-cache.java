3c03020a90620fcd9b35182eb5e10cb01818d227
Kshitij Narkhede <110620887+Kshitijnark@users.noreply.github.com>
2023-10-02 17:25:10 +0530

    Time: 43 ms (97.69%), Space: 127.1 MB (26.33%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.java
+++ 0460-lfu-cache/0460-lfu-cache.java
@@ -26,12 +26,20 @@ class Node{
             newnode.next=next;
             next.prev=newnode;
          }
+/*
+addNode--
+we will add node right after head ... when frequency is upadated or new put
+ */
          Node removenode(){
              Node node=tail.prev;
              node.prev.next=tail;
              tail.prev=node.prev;
              return node;
          }
+/*
+removenode
+we will remove node tail.prev to delete it when there will be new put 
+ */
          Node removeNode(Node delnode){
              Node prev=delnode.prev;
              Node next=delnode.next;
@@ -39,6 +47,10 @@ class Node{
              next.prev=prev;
              return delnode;
          }
+/*
+removenode(v)--
+when freq is updated we ahve to update it in freqmap shift link to addition frequency DLL
+ */
          boolean isEmpty(){
              if(head.next==tail)return true;
              return false;
@@ -64,40 +76,40 @@ class LFUCache {
     }
     
     public int get(int key) {
-        if(lfucache.get(key)==null){
+        if(lfucache.get(key)==null){    //if key-node doesnt present it map return -1
             return -1;
         }
-        Node v=lfucache.get(key);
-        freqlist.get(v.freq).removeNode(v);
-        if(freqlist.get(v.freq).isEmpty()){
-            if(minfreq==v.freq)
+        Node v=lfucache.get(key);          // Node v = node having key 
+        freqlist.get(v.freq).removeNode(v); // from freqlist we will delete it having its freq as key
+        if(freqlist.get(v.freq).isEmpty()){   // if previous frequency is null now then 
+            if(minfreq==v.freq)                //if it was minfreq then it will updated
             minfreq=v.freq+1;
         }
-        v.freq+=1;
-        if(freqlist.get(v.freq)==null){
-            DLL d=new DLL();
-            d.addNode(v);
-            freqlist.put(v.freq,d);
+        v.freq+=1;                              //now we will add it to new freq
+        if(freqlist.get(v.freq)==null){         // if it is null we have to create new DLL
+            DLL d=new DLL();         
+            d.addNode(v);                   // newnode added  after head
+            freqlist.put(v.freq,d);         // freqlist updated --- changed from one freq to other
         }
         else{
-            freqlist.get(v.freq).addNode(v);
+            freqlist.get(v.freq).addNode(v);    // if it is not null we simply add node 
         }
         return v.val;
 
     }
-    
+
     public void put(int key, int value) {
         if(capacity==0)
         return ;
-         if(lfucache.get(key)!=null){
-        Node v=lfucache.get(key);
-        freqlist.get(v.freq).removeNode(v);
-        if(freqlist.get(v.freq).isEmpty()){
+         if(lfucache.get(key)!=null){      // if it is  present in lfucache map   
+        Node v=lfucache.get(key);           // v= node having key
+        freqlist.get(v.freq).removeNode(v); // removing node from its place
+        if(freqlist.get(v.freq).isEmpty()){ //updating minfreq
             if(minfreq==v.freq)
             minfreq=v.freq+1;
         }
-        v.freq+=1;
-        if(freqlist.get(v.freq)==null){
+        v.freq+=1;                             // freq is updated now 
+        if(freqlist.get(v.freq)==null){         // adding node to its new place of new freq
             DLL d=new DLL();
             d.addNode(v);
             freqlist.put(v.freq,d);
@@ -105,24 +117,24 @@ class LFUCache {
         else{
             freqlist.get(v.freq).addNode(v);
         }
-        v.val=value;
+        v.val=value;                        // new value updated
     }
     else{
-        if(lfucache.size()==capacity){
-            Node v=freqlist.get(minfreq).removenode();
-            lfucache.remove(v.key);
+        if(lfucache.size()==capacity){                  // if capacity is full
+            Node v=freqlist.get(minfreq).removenode();  // we will remove lfu
+            lfucache.remove(v.key);                     // also from map
         }
-        Node newnode=new Node(key,value);
-        lfucache.put(key,newnode);
-        if(freqlist.get(1)!=null){
-            freqlist.get(1).addNode(newnode);
+        Node newnode=new Node(key,value);               // new key node
+        lfucache.put(key,newnode);                         //added in map
+        if(freqlist.get(1)!=null){                      //its freq is 1 so we have to add it in freq 1
+            freqlist.get(1).addNode(newnode);           // if no element with 1 ,then new DLL we have to make
         }
         else{
 	     	DLL d=new DLL();
 	     	d.addNode(newnode);
 	     	freqlist.put(1,d);
 	    }
-        minfreq=1;
+        minfreq=1;              // new minfreq is 1
 	      
     }
     }
