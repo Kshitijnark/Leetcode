c4f949a7e54589ec6557e0b08ad0535e62fd2d17
Kshitij Narkhede <110620887+Kshitijnark@users.noreply.github.com>
2023-10-06 00:21:33 +0530

    Time: 3 ms (89.72%), Space: 42.5 MB (85.16%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,27 +1,28 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] nums3 = new int[nums1.length];
-
-        for (int i = 0; i < nums3.length ; i++)
-        {
-            nums3[i] =  findNextMaxNum(nums2 , nums1[i]);
-        }
-        return nums3;
-    }
-
-    public static int findNextMaxNum(int[] arr , int n )
-    {
-         int temp = -1 ;
-        for (int i = 0 ; i < arr.length ; i++)
-        {
-            if (arr[i] == n) {
-                while ((i + 1) < arr.length && temp <= n) {
-                    i++;
-                    temp = Math.max(n, arr[i]);
+        int[] nge=new int[nums2.length];
+        for(int i=0;i<nums2.length;i++){
+            for(int j=i;j<nums2.length;j++){
+                if(nums2[j]>nums2[i]){
+                    nge[i]=nums2[j];
+                    break;
                 }
-                break;
+                nge[i]=-1;
             }
         }
-        return (temp == n) ? -1 : temp ;
+        int[] ans=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            int index=-1;
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j]){
+                    index=j;
+                    ans[i]=nge[index];
+                    break;
+                }
+                
+                
+            }
+        }
+        return ans;
     }
 }
