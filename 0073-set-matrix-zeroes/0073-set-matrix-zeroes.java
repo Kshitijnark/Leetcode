caab78ac34c40f94f559a9b86ed49acffad60235
Kshitij Narkhede <110620887+Kshitijnark@users.noreply.github.com>
2023-10-24 09:13:12 +0530

    Time: 1 ms (89.86%), Space: 44.2 MB (94.19%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,23 +1,53 @@
 class Solution {
+    public void makerowzero(int[][] matrix,int i){
+        for(int j=1;j<matrix[0].length;j++){
+            matrix[i][j]=0;
+        }
+    }
+    public void makecolzero(int[][] matrix,int i){
+        for(int j=1;j<matrix.length;j++){
+            matrix[j][i]=0;
+        }
+    }
     public void setZeroes(int[][] matrix) {
-         int rows = matrix.length;
-        int cols = matrix[0].length;
-        boolean[] zeroRows = new boolean[rows];
-        boolean[] zeroCols = new boolean[cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == 0) {
-                    zeroRows[i] = true;
-                    zeroCols[j] = true;
-                }
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
             }
         }
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (zeroRows[i] || zeroCols[j]) {
-                    matrix[i][j] = 0;
-                }
+
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (matrix[0][i] == 0) {
+                firstRowZero = true;
+                break;
             }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                    if(matrix[i][j]==0){
+                        matrix[0][j]=0;
+                        matrix[i][0]=0;
+                    }
+                    
+            }
+        }
+        for(int i=1;i<matrix.length;i++){
+            if(matrix[i][0]==0)makerowzero(matrix,i);
+        }
+        for(int i=1;i<matrix[0].length;i++){
+            if(matrix[0][i]==0)makecolzero(matrix,i);
+        }
+        if (firstColZero) {
+            makecolzero(matrix,0);
+            }
+         if (firstRowZero) {
+            makerowzero(matrix,0);
+        }
+        }
+
+       
+
     }
-}
-}
